6f1f7f4a2825f03808d53c0f0d2c44efdad11cf4
felko <baxbaxwalanuksiwe@gmail.com>
2016-10-01 17:45:55 +0200

    Added reader function to ArrowReader and changed asks signature

--- Control/Arrow/Reader/Class.hs
+++ Control/Arrow/Reader/Class.hs
@@ -1,15 +1,24 @@
-{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE
+    Arrows
+  , FunctionalDependencies
+  #-}
 
 module Control.Arrow.Reader.Class where
 
 import Control.Arrow
 
 class Arrow a => ArrowReader r a | a -> r where
+    reader :: (b -> r -> c) -> a b c
+    reader f = proc x -> do
+        r <- ask -< ()
+        returnA -< f x r
+
     ask :: a () r
+    ask = reader (\ _ r -> r)
+
     local :: (r -> r) -> a b c -> a b c
 
-asks :: ArrowReader r a => (r -> b) -> a () b
-asks = asksA . arr
-
-asksA :: ArrowReader r a => a r b -> a () b
-asksA = (ask >>>)
+asks :: ArrowReader r a => a (r -> b) b
+asks = proc f -> do
+    r <- ask -< ()
+    returnA -< f r
