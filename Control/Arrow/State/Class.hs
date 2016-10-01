ceae65781bdb6e5c097698c246e8ca8e690a64ec
felko <baxbaxwalanuksiwe@gmail.com>
2016-10-01 17:37:01 +0200

    Added state function to class ArrowState

--- Control/Arrow/State/Class.hs
+++ Control/Arrow/State/Class.hs
@@ -9,8 +9,19 @@ module Control.Arrow.State.Class where
 import Control.Arrow
 
 class Arrow a => ArrowState s a | a -> s where
+    state :: (b -> s -> (c, s)) -> a b c
+    state f = proc x -> do
+        s <- get -< ()
+        (y, s') <- returnA -< f x s
+        put -< s'
+        returnA -< y
+
     get :: a () s
+    get = state (\ _ s -> (s, s))
+
     put :: a s ()
+    put = state (\ s _ -> ((), s))
+
 
 gets :: ArrowState s a => a (s -> b) b
 gets = proc f -> do
